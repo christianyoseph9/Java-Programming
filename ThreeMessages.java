9597cde146f0f1d8aaf3592c969115a19cb0d984
Yoseph Christian <63234711+christianyoseph9@users.noreply.github.com>
2021-01-16 19:16:43 +0700

    Add files via upload

---
+++ ThreeMessages.java
@@ -0,0 +1,8 @@
+public class ThreeMessages{
+    public static void main(String args[]) {
+        // TODO code application logic here
+        System.out.println("Hello!");
+        System.out.println("Java is Fun");
+        System.out.println("Come on!");
+    }
+}
