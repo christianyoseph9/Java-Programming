f8902cf8530a41e5fe4e48b4c7d824e2d713ecb0
Yoseph Christian <63234711+christianyoseph9@users.noreply.github.com>
2021-01-16 19:17:11 +0700

    Update ThreeMessages.java

--- ThreeMessages.java
+++ ThreeMessages.java
@@ -1,6 +1,6 @@
 public class ThreeMessages{
     public static void main(String args[]) {
-        // TODO code application logic here
+        //Print three messages
         System.out.println("Hello!");
         System.out.println("Java is Fun");
         System.out.println("Come on!");
